bb9073b1c2a8274ebc2594e290d7dd099bac91eb
atilianonegrin <atiliano98@hotmail.com>
2026-08-06 18:27:35 -0300

    Add team and player navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './features/home/home-page.component';
 import { DivisionalAPageComponent } from './features/competitions/divisional-a-page.component';
+import { TeamsPageComponent } from './features/teams/teams-page.component';
+import { TeamDetailPageComponent } from './features/teams/team-detail-page.component';
+import { PlayerDetailPageComponent } from './features/players/player-detail-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent, title: 'Liga Canaria de Futsal' },
@@ -9,5 +12,8 @@ export const routes: Routes = [
     component: DivisionalAPageComponent,
     title: 'Divisional A | Liga Canaria de Futsal'
   },
+  { path: 'temporadas/:year/equipos', component: TeamsPageComponent, title: 'Equipos | Liga Canaria de Futsal' },
+  { path: 'temporadas/:year/equipos/:teamId', component: TeamDetailPageComponent, title: 'Equipo | Liga Canaria de Futsal' },
+  { path: 'temporadas/:year/equipos/:teamId/jugadores/:playerId', component: PlayerDetailPageComponent, title: 'Jugador | Liga Canaria de Futsal' },
   { path: '**', redirectTo: '' }
 ];
